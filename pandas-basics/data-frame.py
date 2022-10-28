5b9770f93125f14e44cb6f218192b23f2bbcacdb
rbverissimo <rverissimo.dev@gmail.com>
2022-10-28 17:15:40 -0300

    understanding the data frame

--- pandas-basics/data-frame.py
+++ pandas-basics/data-frame.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 column = [1000, 1001, 1002, 1003]
+titled_column = {'numbers': column}  # the column list will be appended into the "numbers" key in the dict
+data = pd.DataFrame(titled_column)
+print(data)  # this organizes the column list in a table
 
-pd.DataFrame(column)
 
