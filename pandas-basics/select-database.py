07abc082a08193bf0af498b21b25f750ecab3600
rbverissimo <rverissimo.dev@gmail.com>
2022-11-02 11:32:29 -0300

    filtering data with the dataframe

--- pandas-basics/select-database.py
+++ pandas-basics/select-database.py
@@ -8,8 +8,14 @@ customers_data = pd.read_sql('select * from customers', connection)
 
 print(customers_data)
 print("\n")
+
+# applying a filter to the customers_data dataframe
 journalist_dataframe = customers_data[customers_data['occupation'] == 'Journalist']
 print(journalist_dataframe)
+
+# replacing Journalist for Communication
+communication_dataframe = journalist_dataframe.replace('Journalist', 'Communication')
+print("\n", communication_dataframe)
 
 
 connection.close()
